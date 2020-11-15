0089b2becd26aacef3e175e6064745a7d8566752
PauloRolim <pauloerb@hotmail.com>
2020-11-14 21:36:49 -0300

    resposta a lista de exercicios

---
+++ lab04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+    int var1 = 10;
+    int var2 = 20;
+
+    for (int i = 0; i < 5; i++)
+    {
+        var1++;
+    }
+
+    printf("Resuldado: %d \n", var1);
+
+    return 0;
+}
